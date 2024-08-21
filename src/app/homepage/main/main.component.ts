53115dc8f769e77fde3e65a45fcda60fc8592e46
KantihiSubramanyam <subramanyamkanithi@gmail.com>
2024-08-21 14:47:10 +0530

    sidebar and routing for main completed

--- src/app/homepage/main/main.component.ts
+++ src/app/homepage/main/main.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Router } from 'express';
+import { DashboardComponent } from "./dashboard/dashboard.component";
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, DashboardComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
