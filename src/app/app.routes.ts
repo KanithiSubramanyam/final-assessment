5c7ce4f0b0e27fa562b827bc7e375328d9cfd217
KantihiSubramanyam <subramanyamkanithi@gmail.com>
2024-08-21 15:17:31 +0530

    changes in sidebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,9 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { DashboardComponent } from './homepage/main/dashboard/dashboard.component';
 import { UserManagementComponent } from './homepage/main/user-management/user-management.component';
+import { UsersComponent } from './homepage/main/user-management/users/users.component';
+import { PermissionsComponent } from './homepage/main/user-management/permissions/permissions.component';
+import { RolesComponent } from './homepage/main/user-management/roles/roles.component';
 
 
 export const routes: Routes = [
@@ -11,7 +14,9 @@ export const routes: Routes = [
     { path: '', component: HomepageComponent, children: [
         { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
         { path: 'dashboard', component: DashboardComponent },
-        { path: 'userManagement', component: UserManagementComponent },
+        { path: 'users', component: UsersComponent },
+        { path: 'permissions', component: PermissionsComponent },
+        { path: 'roles', component: RolesComponent },
     ]},
     { path: '**', component: NotFoundComponent },
 ];
