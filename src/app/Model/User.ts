e54cf77ba334b24cbeaa767e5defc5a71b73f841
gnaneswara123 <gnaneswar.petla@gmail.com>
2024-08-21 16:51:57 +0530

    loginpage

---
+++ src/app/Model/User.ts
@@ -0,0 +1,20 @@
+export class User {
+    constructor(
+        // public id: string,
+        public firstName: string,
+        public lastName: string,
+        public email: string,
+        public password: string,
+        
+        // public role: string,
+        // private _token: string,
+        // private _expiresIn: Date
+    ) {}
+
+    // get token() {
+    //     if (!this._expiresIn || new Date() > this._expiresIn) {
+    //         return null;
+    //     }
+    //     return this._token;
+    // }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { CommonModule } from '@angular/common';
+
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,LoginComponent],
+  imports: [RouterOutlet,LoginComponent ,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'final-assessment';
+
+  
 }
+
