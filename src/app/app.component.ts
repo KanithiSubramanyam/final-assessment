492a1a5cb970e1a803825d9df819bfc336d6dbc2
KantihiSubramanyam <subramanyamkanithi@gmail.com>
2024-08-21 14:48:15 +0530

    changes in sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { SidebarComponent } from './homepage/sidebar/sidebar.component';
+import { TopbarComponent } from './homepage/topbar/topbar.component';
+import { MainComponent } from './homepage/main/main.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,LoginComponent],
+  imports: [RouterOutlet,LoginComponent, SidebarComponent, TopbarComponent, MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'final-assessment';
 }
+ 
