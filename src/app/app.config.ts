5b2073e4fc2d5cefe9ddde6a7e6205b6fc92dde3
KantihiSubramanyam <subramanyamkanithi@gmail.com>
2024-08-23 15:35:35 +0530

    interceptors added to the file

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,12 +3,12 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { HTTP_INTERCEPTORS, provideHttpClient, withFetch } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withFetch, withInterceptorsFromDi } from '@angular/common/http';
 import { AuthInterceptorService } from './Services/auth-interceptor.service';
 
 export const appConfig: ApplicationConfig = {
   //to use httpClient need to import provideHttpClient
-  providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(), provideHttpClient(withFetch()),
+  providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(), provideHttpClient(withFetch()), provideHttpClient(withInterceptorsFromDi()), 
     {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true}
   ]
 };
